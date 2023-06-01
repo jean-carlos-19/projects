f3cc19afd43b74ee2cab6af1c78829c2cb081fa3
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-01 11:54:16 -0500

    Creado la carpeta golbal y con su apartado de database y el archivo postgresql.ts

---
+++ backend/global/database/index.ts
@@ -0,0 +1,1 @@
+export {Postgresql} from "./postgresql";
---
+++ backend/global/database/postgresql.ts
@@ -0,0 +1,31 @@
+import { Client,ClientConfig } from "pg";
+
+const CONFIG_PG = Object.freeze<ClientConfig>({
+    host:process.env.HOST,
+    port:Number.parseInt(`${process.env.PORT}`),
+    database:process.env.DATABASE,
+    user:process.env.USER,
+    password:process.env.PASSWORD
+});
+
+class Postgresql{
+    
+    private static postgresql:Postgresql;
+    private client!: Client;
+
+    private constructor (){
+        this.client = new Client(CONFIG_PG);
+    }
+
+    public static get_postgresql():Postgresql{
+        if(this.postgresql === undefined ) this.postgresql = new Postgresql();
+        return this.postgresql;
+    }
+
+    public async connect():Promise<Client>{
+        await this.client.connect();
+        return this.client;
+    }
+
+}
+export {Postgresql}
