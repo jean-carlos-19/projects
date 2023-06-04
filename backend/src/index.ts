b08a9633f532cceca2b975946869bce388b08bfe
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-03 21:32:47 -0500

    Actualizado el archivo principal index.ts donde se añadio la ruta de la carpeta proyecto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import { Postgresql } from "global/database";
-import express from "express";
+import express, { Request, Response } from "express";
 import morgan from "morgan";
 import http from "http";
 import { ROUTE_PROJECT } from "projects/infrastructure/route";
-
 
 const APP = express();
 const PORT: number = 3000;
@@ -18,6 +17,12 @@ APP.use(express.json());
 APP.use(morgan("dev"));
 
 APP.use("/project", ROUTE_PROJECT);
+APP.use("*", (request: Request, response: Response) => {
+  response.json({
+    id_response: 404,
+    message_response: "Page not found :C",
+  });
+});
 
 const SERVER = http.createServer(APP);
 
