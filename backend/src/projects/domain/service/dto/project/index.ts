13fba618547dc41028352d223859ae1cfca4d5cf
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-03 21:24:41 -0500

    Creado el archivo list.dto.ts que devuelve los parametros obtenido de la db y actualizando el archivo index.ts donde se exporta el archivo list.dto.ts

--- backend/src/projects/domain/service/dto/project/index.ts
+++ backend/src/projects/domain/service/dto/project/index.ts
@@ -1,1 +1,2 @@
 export type { create_dto } from "./create.dto";
+export type { list_dto } from "./list.dto";
---
+++ backend/src/projects/domain/service/dto/project/list.dto.ts
@@ -0,0 +1,10 @@
+interface list_dto {
+  title: string;
+  description: string;
+  conclusion: string;
+  category: string;
+  url_image: string;
+  url_demo: string;
+  url_github: string;
+}
+export type { list_dto };
