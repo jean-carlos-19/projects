4c99c7f71ee56a89893d678ef0632e027d3f14dd
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-03 21:17:34 -0500

    Creado la carpeta http y el archivo http.ts el cual tiene el objetivo de enviar las respuestas http al cliente

--- backend/global/http/http.ts
+++ backend/global/http/http.ts
@@ -1,18 +1,26 @@
 import { Response } from "express";
 
-class Http{
-
-    public response(response:Response,id_response:number,message_response:string){
-        response.json({
-            id_response,
-            message_response
-        })
-    }
-    public response_server_failed(response:Response){
-        response.json({
-            id_response:500,
-            message_response:'Error internal server'
-        })
-    }
+class Http {
+  public response_cud(
+    response: Response,
+    id_response: number,
+    message_response: string
+  ) {
+    response.json({
+      id_response,
+      message_response,
+    });
+  }
+  public response_r(response: Response, data: any[]) {
+    response.json({
+      data: data,
+    });
+  }
+  public response_server_failed(response: Response) {
+    response.json({
+      id_response: 500,
+      message_response: "Error internal server",
+    });
+  }
 }
-export {Http}
+export { Http };
