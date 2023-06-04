4bd643050ee0dc0587ec4856f5a55db15d296c24
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-03 21:26:38 -0500

    Actulizado el archivo project.service.ts y agregando la funcion search para buscar los porjectos por su categoria

--- backend/src/projects/domain/service/project.service.ts
+++ backend/src/projects/domain/service/project.service.ts
@@ -1,7 +1,8 @@
 import { Project_Entity } from "projects/domain/entity";
-import { create_dto } from "./dto/project";
+import { create_dto, list_dto } from "./dto/project";
 import { QueryResult } from "pg";
 interface Project_Service {
   create(project: Project_Entity): Promise<QueryResult<create_dto>>;
+  search(category: string): Promise<QueryResult<list_dto>>;
 }
 export { Project_Service };
