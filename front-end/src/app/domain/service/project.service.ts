9826f2e0eb21157801e65bb9c88f3ddabb017ac6
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-10 14:51:43 -0500

    Actualizando el archivo project.repository.ts. Añadiendo la funcion search que permite buscar projectos con su categoria

--- front-end/src/app/domain/service/project.service.ts
+++ front-end/src/app/domain/service/project.service.ts
@@ -1,7 +1,8 @@
 import { AxiosResponse } from "axios";
 import { List_Entity } from "@project/domain/entity";
+import { search_dto } from "../dto";
 interface Project_Service {
-  list(): Promise<AxiosResponse<List_Entity>>;
+  search(name_category:string): Promise<AxiosResponse<search_dto>>;
 }
 
 export type { Project_Service };
