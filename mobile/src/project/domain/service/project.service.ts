e43a34903d44083eba30df66da4b05c3ec6b8097
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-10 23:15:55 -0500

    Actualizado el archivo project.service.ts . Agregando el parametro project al metodo create

--- mobile/src/project/domain/service/project.service.ts
+++ mobile/src/project/domain/service/project.service.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios"
 import {create_dto} from 'project/domain/dto'
+import {Create_Entity} from 'project/domain/entity'
 interface project_service {
-    create():Promise<AxiosResponse<create_dto>>
+    create(project:Create_Entity):Promise<AxiosResponse<create_dto>>
 }
 export type {project_service}
