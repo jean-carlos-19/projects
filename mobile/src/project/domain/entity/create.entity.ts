2f310910e17f8795c612b6a3c383e2eb53623866
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-10 21:42:12 -0500

    Modificado el archivo create.entity.ts y el index.ts. Agregando las caracteristicas para crear los proyectos e importanto el modulo de create.entity.ts en el index.ts

---
+++ mobile/src/project/domain/entity/create.entity.ts
@@ -0,0 +1,11 @@
+interface Create_Entity{
+    title:string;
+    description:string;
+    conclusion:string;
+    category:string;
+    tecnologies:string;
+    url_image:string;
+    url_demo:string;
+    url_github:string;
+}
+export type {Create_Entity};
---
+++ mobile/src/project/domain/entity/index.ts
@@ -0,0 +1,1 @@
+export type {Create_Entity} from "./create.entity";
