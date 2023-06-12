1b7612402edcd5d6c36e07d7e7163b6168766e94
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-12 16:34:36 -0500

    Actualizado el archivo form.tsx. añdido un ciclo for para hacer el recorrido de los campos

--- mobile/atomic/layout/Form.tsx
+++ mobile/atomic/layout/Form.tsx
@@ -1,45 +1,47 @@
 import React from 'react'
-import {Formik} from 'formik'
+import { Formik } from 'formik'
+import { AnyObject, ObjectSchema } from 'yup';
 import { Field_Text } from 'global/atomic/component';
 
-interface form_props{
-    initialValues:any
+interface form_props {
+    initialValues:any;
+    validadion: ObjectSchema<any>;
+    max_values:any;
 }
-const Form = (props:form_props) => {
-    const {initialValues} = props;
-  return (
-    <Formik
-        validateOnMount={true}
-        initialValues={initialValues}
-        onSubmit={(values,actions)=>{
-            console.log(values)
-        }}
-    >
-     {
-        (props)=>{
-            const {values,handleChange,handleBlur} = props;
-            return (      
-                Object.keys(initialValues).map((prop:string,i:number)=>{
-                    console.log(values.title)
-                    return(
-                        <Field_Text 
-                            key={i}
-                            title={`${prop.toLocaleLowerCase()}`} 
-                            sms_error={''} 
-                            value={values[prop]} 
-                            placeholder={`${prop.toLocaleLowerCase()}`} 
-                            editable={true} 
-                            handleChange={handleChange(`${prop}`)} 
-                            handleBlur={handleBlur(`${prop}`)} 
-                            max_length={25} 
-                        />
+const Form = (props: form_props) => {
+    const { initialValues,validadion,max_values } = props;
+    return (
+        <Formik
+            validateOnMount={true}
+            validationSchema={validadion}
+            initialValues={initialValues}
+            onSubmit={(values, actions) => {
+                console.log(values)
+            }}
+        >
+            {
+                (props) => {
+                    const { values,errors, touched, handleChange, handleBlur } = props;
+                    return (
+                        Object.keys(initialValues).map((prop:string,i:number)=>(
+                            <Field_Text
+                                key={i}
+                                title={prop}
+                                sms_error={errors?.[prop] && touched?.[prop] ? errors?.[prop] : ''}
+                                value={values[prop]}
+                                placeholder={`Ej. Myfirst project.`}
+                                editable={true}
+                                handleChange={handleChange(prop)}
+                                handleBlur={handleBlur(prop)}
+                                max_length={max_values[prop]}
+                            />
+                        ))
+                            
                     )
-                })
-            )
-        }
-     }
-    </Formik>
-  )
+                }
+            }
+        </Formik>
+    )
 }
 
-export  {Form}
+export { Form }
