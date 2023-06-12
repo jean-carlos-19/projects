14a02fcd99ea2f217e9788a9d58577e3888d338a
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-12 16:31:46 -0500

    Actualizado el archivo validation.ts. Añadiendo las propiedades de validacion para cada uno de los campos. valor maximo y campos requeridos

--- mobile/src/project/presentation/home/validation.ts
+++ mobile/src/project/presentation/home/validation.ts
@@ -1,11 +1,21 @@
 import {object,string} from 'yup'
-const validation = object({
-    title:string().required(),
-    description:string().required(),
-    conclusion:string().required(),
-    category:string().required(),
-    tecnologies:string().required(),
-    demo:string().required(),
-    github:string().required(),
+
+const MAX_VALUE = Object.freeze({
+    title:50,
+    description:255,
+    conclusion:255,
+    category:255,
+    tecnologies:255,
+    demo:255,
+    github:255
 })
-export {validation}
+const validation = object({
+    title:string().required('title is a required field').max(MAX_VALUE.title,'value exceeded'),
+    description:string().required('descripction is a required field').max(MAX_VALUE.description,'value exceeded'),
+    conclusion:string().required('conclusion is a required field').max(MAX_VALUE.conclusion,'value exceeded'),
+    category:string().required('category is a required field').max(MAX_VALUE.category,'value exceeded'),
+    tecnologies:string().required('tecnologies is a required field').max(MAX_VALUE.tecnologies,'value exceeded'),
+    demo:string().required('demo is a required field').max(MAX_VALUE.demo,'value exceeded'),
+    github:string().required('github is a required field').max(MAX_VALUE.github,'value exceeded'),
+})
+export {validation,MAX_VALUE}
