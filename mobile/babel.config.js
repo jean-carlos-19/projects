c0468c42df674ea6cf494ea98c44123620f4d9fc
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-11 21:44:06 -0500

    Añadido dependencias para el proyecto en el archivo babel.config.js para los alias al momento de sun compilacion a js

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins:[
+      [
+        'module-resolver',
+        {
+          'root':["."],
+          'alias':{
+              'global/atomic/element':'./atomic/element',
+              'global/atomic/component':'./atomic/component',
+              'global/atomic/layout':'./atomic/layout',
+  
+              'project/domain/entity':'./src/project/domain/entity',
+              'project/domain/dto':'./src/project/domain/dto',
+              'project/domain/service':'./src/project/domain/service',
+  
+              'project/infrastructure/http':'./src/project/infrastructure/http',
+              'project/infrastructure/repository':'./src/project/infrastructure/repository',
+  
+              'project/presentation':'./src/project/presentation/Route.tsx',
+          }
+        }
+      ]
+    ]
   };
 };
