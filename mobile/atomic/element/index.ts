e3442a948f7f784bb6657fe08a1719d05139738c
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-11 19:19:45 -0500

    importando los modulos al index.ts para ser utilizados por la capa presentacion

---
+++ mobile/atomic/element/index.ts
@@ -0,0 +1,5 @@
+export {Input_Text} from "./Input_Text";
+export {Label} from "./Label";
+export {Subtitle} from "./Subtitle";
+export {Text_Small} from "./Text_Small";
+export {Tittle_1} from "./Tittle_1";
