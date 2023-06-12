b5112799a3d1842afcaa119e44161bd113ca9201
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-12 16:26:39 -0500

    Modificado el archivo Field_Text.tsx y field.styles.ts. Agregando los estilos para los campos.

--- mobile/atomic/component/Field_Text.tsx
+++ mobile/atomic/component/Field_Text.tsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { NativeSyntheticEvent, TextInputFocusEventData, View } from 'react-native'
 import { Label,Text_Small, Input_Text } from 'global/atomic/element'
 import {field_styles} from './style/field.styles'
 
 interface field_props{
     title:string;
-    sms_error:string;
-    
-    value:any;
+    sms_error:any;
+    value:string;
     placeholder:string;
     editable:boolean;
-    handleChange:(field: any) => void | ((e: string | React.ChangeEvent<any>) => void)
-    handleBlur:(field: any) => void | ((e: string | React.ChangeEvent<any>) => void)
-    
     max_length:number;
+    handleChange:(((text: string) => void) | undefined);
+    handleBlur:((e: NativeSyntheticEvent<TextInputFocusEventData>) => void) | undefined;
 }
 const Field_Text = (props:field_props) => {
     const {title,sms_error,value,placeholder,editable,handleChange,handleBlur,max_length} = props;
     return (
     <View style={field_styles.container}>
-      <View>
+      <View style={field_styles.header}>
         <Label style={field_styles.title} text={title} />
-        <Text_Small text={sms_error} />
+        <Text_Small style={field_styles.sms_error} text={sms_error}  />
       </View>
       <Input_Text 
         backgroundColor={field_styles.field}
@@ -31,7 +29,7 @@ const Field_Text = (props:field_props) => {
         handleChange={handleChange} 
         handleBlur={handleBlur}      
         />
-      <Text_Small style={field_styles.sms_error} text={`${value} / ${max_length}`} />
+      <Text_Small style={[field_styles.lenght_error,(value.length > max_length) ? {color:"#F46E6E"}:{}]} text={`${value.length} / ${max_length}`} />
     </View>
   )
 }
--- mobile/atomic/component/style/field.styles.ts
+++ mobile/atomic/component/style/field.styles.ts
@@ -1,16 +1,28 @@
 import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 const field_styles =  StyleSheet.create({
     container:{
         width:"100%",
+        height:RFValue(100),
         flexDirection:"column",
         alignItems:"flex-start",
+        justifyContent:"space-between",
+        marginTop:RFValue(24)
+    },
+    header:{
+        width:"100%",
+        flexDirection:'row',
+        justifyContent:"space-between"
     },
     field:{
         color:"#9B9999",
         backgroundColor:"rgba(131, 126, 126, 0.3)"
     },
     sms_error:{
+        color:"#F46E6E",
+    },
+    lenght_error:{
         width:"100%",
         color:"#9B9999",
         textAlign:"right"
