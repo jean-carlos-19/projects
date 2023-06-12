96a5c35ec162e827b566d599860e805ee8fde726
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-12 16:36:33 -0500

    Actualizado el archivo home.tsx. añadiendo propiedades max_values y validation para el componente form

--- mobile/src/project/presentation/home/Home.tsx
+++ mobile/src/project/presentation/home/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { View } from 'react-native'
+import { Tittle_1 } from 'global/atomic/element'
 import {Form} from 'global/atomic/layout'
 import {STYLES} from './style/styles'
-import { Tittle_1 } from 'global/atomic/element'
+import {validation,MAX_VALUE} from './validation'
+
 const Home = () => {
   return (
     <View style={STYLES.container}>
       <Tittle_1 text={'Create your first projects'} />
-      <Form 
+      <Form
+        max_values={MAX_VALUE} 
+        validadion={validation}
         initialValues={{
           title:"",
           description:"",
