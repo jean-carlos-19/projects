689b644d55e752c549701f678738391ff270feb9
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-11 19:45:42 -0500

    Actualizado el archivo Input_Text.tsx. Añadiendo atributos value, editable, handleChange, handleBlur

--- mobile/atomic/element/Input_Text.tsx
+++ mobile/atomic/element/Input_Text.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { TextInput } from 'react-native'
+import { NativeSyntheticEvent, TextInput, TextInputChangeEventData, TextInputFocusEventData } from 'react-native'
 
 interface input_props{
+  value:any;
   placeholder:string;
+  editable:boolean;
+  handleChange:((e: NativeSyntheticEvent<TextInputChangeEventData>) => void) | undefined
+  handleBlur:((e: NativeSyntheticEvent<TextInputFocusEventData>) => void) | undefined;
 }
 const Input_Text = (props:input_props) => {
-  const {placeholder} = props;
+  const {placeholder,editable,handleBlur,handleChange,value} = props;
   return (
     <TextInput 
       placeholder={placeholder}
+      value={value}
+      editable={editable}
+      onChange={handleChange}
+      onBlur={handleBlur}
     />
   )
 }
