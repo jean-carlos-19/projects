1fe83ee985f49263a83b059690e58f57394e99f0
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-12 19:39:18 -0500

    Actualizado archivo useHome.ts. Agregado los valores iniciales para el formulario y camposç

--- mobile/src/project/presentation/home/hooks/useHome.ts
+++ mobile/src/project/presentation/home/hooks/useHome.ts
@@ -1,6 +1,48 @@
+import { useState } from "react";
 
+const values_project = {
+    title:"",
+    description:"",
+    conclusion:"",
+    category:"",
+    tecnologies:"",
+    demo:"",
+    github:"",   
+}
+const fields = {
+    title:{
+        label:"",
+        placeholder:""
+    },
+    description:{
+        label:"",
+        placeholder:""
+    },
+    conclusion:{
+        label:"",
+        placeholder:""
+    },
+    category:{
+        label:"",
+        placeholder:""
+    },
+    tecnologies:{
+        label:"",
+        placeholder:""
+    },
+    demo:{
+        label:"",
+        placeholder:""
+    },
+    github:{
+        label:"",
+        placeholder:""
+    }   
+}
 const useHome =()=>{
-
+    const [initialValues]= useState(values_project);
+    const [fields]= useState(values_project);
+    return {initialValues,fields};
 };
 
 export {useHome}
