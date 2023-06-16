d2b986c02e6887e450c8369486b5079277993504
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-16 18:15:38 -0500

    Modificado project.repository.ts . Agregando el repositorio para agregar el metdo para crear los proyectos

--- mobile/src/project/presentation/home/hooks/useHome.ts
+++ mobile/src/project/presentation/home/hooks/useHome.ts
@@ -1,5 +1,10 @@
 import { useCallback, useState } from "react";
 import { Create_Entity } from "project/domain/entity";
+import {Project_Repository} from 'project/infrastructure/repository'
+import { AxiosResponse } from "axios";
+import { create_dto } from "project/domain/dto";
+
+const repository:Project_Repository = Project_Repository.get_repository();
 const values_project: Create_Entity = {
   title: "",
   description: "",
@@ -17,8 +22,12 @@ const useHome = (
   set_message:(sms:string)=>void,
 ) => {
   const [initialValues] = useState(values_project);
-  const create_project = useCallback((datos: Create_Entity) => {
-    console.log(datos);
+
+  const create_project = useCallback(async(datos: Create_Entity) => {
+  
+      const respuesta:AxiosResponse<create_dto, any> = await repository.create(datos);
+      const {id_message,message_response} = respuesta.data.data;
+      console.log(id_message,message_response)
   }, []);
 
   return { initialValues, create_project };
