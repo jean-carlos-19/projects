91336da98d953dec539213457b35465d89c3472b
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-16 17:49:14 -0500

    Modificado el archivo state.componet.ts. añadiendo la funcion hidden_menssage para ocultar los mesnajes de error o de exito

--- mobile/global/state/state.component.ts
+++ mobile/global/state/state.component.ts
@@ -2,9 +2,14 @@ import { useState } from "react"
 
 const useStateGlobal =()=>{
     const [loading,set_loading] = useState(false);
-    const [message,set_message] = useState();
-    const [error,set_error] = useState();
+    const [message,set_message] = useState('');
+    const [error,set_error] = useState('');
 
-    return {loading,set_loading,message,set_message,error,set_error}
+    const hidden_menssage =()=>{
+        set_error('');
+        set_message('');
+    }
+
+    return {loading,set_loading,message,set_message,error,set_error,hidden_menssage}
 }
 export {useStateGlobal}
