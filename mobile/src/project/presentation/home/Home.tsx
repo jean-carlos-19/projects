50c134a74962926e77ff893397e05d25bac680e7
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-19 13:15:59 -0500

    Modificado el archivo Home.tsx. Añadiendo el hook global para manejar los mensaje de error, exito o loading junto a los componete de modal y loading

--- mobile/src/project/presentation/home/Home.tsx
+++ mobile/src/project/presentation/home/Home.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView,StatusBar } from "react-native";
 import { Tittle_1 } from "global/atomic/element";
-import { Progress_Bar, Loading } from "global/atomic/component";
+import { Progress_Bar, Loading,Modal } from "global/atomic/component";
 import { Form } from "global/atomic/layout";
 import { validation, MAX_VALUE } from "./validation";
 import { styles } from "./style/styles";
 import { useHome, useProgressBar } from "./hooks";
-import {useStateGlobal} from 'global/state'
+import { useStateGlobal } from "global/state";
 
 const Home = () => {
-  const {set_error,loading, set_loading,set_message} = useStateGlobal();
-  const { initialValues, create_project } = useHome(set_loading,set_error,set_message);
+  const {error, set_error, loading, set_loading, message,set_message,hidden_menssage } = useStateGlobal();
+  const { initialValues, create_project } = useHome(
+    set_loading,
+    set_error,
+    set_message
+  );
   const { items, position, position_location } = useProgressBar(2);
   return (
     <ScrollView style={styles.scroll}>
+      <StatusBar backgroundColor={"#0F172A"} />
       <Loading message="Subiendo el proyecto" state={loading} />
+      <Modal sms={message} type={"Success"} close_modal={hidden_menssage} />
+      <Modal sms={error} type={"Error"} close_modal={hidden_menssage} />
       <View style={styles.container}>
         <Tittle_1 text={"Create your first projects"} />
         <Progress_Bar
