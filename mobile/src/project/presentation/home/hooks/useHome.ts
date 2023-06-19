9974370aaab6f51abba0c0c116fef656c32db21c
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-19 13:13:25 -0500

    Modificado el archivo useHome.ts. Añadiendo las funciones para mostrar los mensajes de exito o error y la funcion de efecto de loading

--- mobile/src/project/presentation/home/hooks/useHome.ts
+++ mobile/src/project/presentation/home/hooks/useHome.ts
@@ -1,10 +1,10 @@
 import { useCallback, useState } from "react";
 import { Create_Entity } from "project/domain/entity";
-import {Project_Repository} from 'project/infrastructure/repository'
+import { Project_Repository } from "project/infrastructure/repository";
 import { AxiosResponse } from "axios";
 import { create_dto } from "project/domain/dto";
 
-const repository:Project_Repository = Project_Repository.get_repository();
+const repository: Project_Repository = Project_Repository.get_repository();
 const values_project: Create_Entity = {
   title: "",
   description: "",
@@ -17,17 +17,24 @@ const values_project: Create_Entity = {
 };
 
 const useHome = (
-  set_loading:(state:boolean)=>void,
-  set_error:(sms:string)=>void,
-  set_message:(sms:string)=>void,
+  set_loading: (state: boolean) => void,
+  set_error: (sms: string) => void,
+  set_message: (sms: string) => void
 ) => {
   const [initialValues] = useState(values_project);
 
-  const create_project = useCallback(async(datos: Create_Entity) => {
-  
-      const respuesta:AxiosResponse<create_dto, any> = await repository.create(datos);
-      const {id_message,message_response} = respuesta.data.data;
-      console.log(id_message,message_response)
+  const create_project = useCallback(async (datos: Create_Entity) => {
+    set_loading(true);
+    try {
+      const respuesta: AxiosResponse<create_dto, any> = await repository.create(
+        datos
+      );
+      const { id_response, message_response } = respuesta.data;
+      set_message(message_response)
+    } catch (error) {
+      set_error('Error en el servidor al crear el proyecto')
+    }
+    set_loading(false);
   }, []);
 
   return { initialValues, create_project };
