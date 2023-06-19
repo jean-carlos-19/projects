cbd1fc91caefba7d41e6d8be2858dd3668eb57e8
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-19 15:20:03 -0500

    Modificado el archivo project.repository.ts: Agregado la variable virtual URL_API

--- mobile/src/project/infrastructure/repository/project.repository.ts
+++ mobile/src/project/infrastructure/repository/project.repository.ts
@@ -3,6 +3,8 @@ import { Create_Entity } from "project/domain/entity";
 import { Http } from "project/infrastructure/http";
 import { create_dto } from "project/domain/dto";
 import { AxiosResponse } from "axios";
+import {API_URL} from '@env'
+
 
 class Project_Repository implements project_service {
   public http: Http = new Http();
@@ -11,7 +13,11 @@ class Project_Repository implements project_service {
   public create = async (
     project: Create_Entity
   ): Promise<AxiosResponse<create_dto, any>> => {
-    return await this.http.post("http://192.168.100.7:3001/project/create", project, this.http.header());
+    return await this.http.post(
+      `${API_URL}/project/create`,
+      project,
+      this.http.header()
+    );
   };
 
   private constructor() {}
