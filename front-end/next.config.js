f3d96b62b1d7a3b24caf651f8458584ad5aea710
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-20 15:42:12 -0500

    Modificado el archivo de configuracion nex-config.json: añadido as variables de entorno

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        REACT_URL_API_LOCAL:'http://192.168.100.7:3001',
+        REACT_URL_LOCAL:'http://192.168.100.7',
+        REACT_URL_API_REMOTE:'https://api-piguavesoft.onrender.com'
+    }
+};
 
 module.exports = nextConfig;
