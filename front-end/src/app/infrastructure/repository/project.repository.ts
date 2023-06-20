919945ec6298c6462d1acdccf363d9e52f915862
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-19 23:57:35 -0500

    Modificado el archivo project.repository.ts: Añdiendo la variable virtual REACT_URL_API

--- front-end/src/app/infrastructure/repository/project.repository.ts
+++ front-end/src/app/infrastructure/repository/project.repository.ts
@@ -15,7 +15,7 @@ class Project_Repository implements Project_Service {
   }
   public async search(name_category:string): Promise<AxiosResponse<search_dto>> {
     return await this.http.get<search_dto>(
-      `http://192.168.100.7:3001/project/search?category=${name_category}`,
+      `${process.env.REACT_URL_API}/project/search?category=${name_category}`,
       this.http.header_get()
     );
   }
