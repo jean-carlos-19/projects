6117931456b21040f8ad4b02afb5440f4bfdc73d
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-24 15:52:12 -0500

    modificado el index.ts de componet: importando el componente Not_Found

--- front-end/src/app/presentation/component/index.ts
+++ front-end/src/app/presentation/component/index.ts
@@ -5,3 +5,4 @@ export { List } from "./List";
 export { Loading } from "./Loading";
 export { Footer } from "./Footer";
 export { Social_Media } from "./Social_Media";
+export {Not_Found} from "./Not_Found";
